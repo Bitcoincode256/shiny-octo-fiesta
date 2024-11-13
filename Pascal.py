cf8eb1ec842be10ed0ec0f95f63ab036cce7094f
Bitcoincode256 <160171349+Bitcoincode256@users.noreply.github.com>
2024-11-13 15:09:57 +0100

    Add files via upload

---
+++ Pascal.py
@@ -0,0 +1,93 @@
+import re
+#balance
+begin
+auto balance de compte = Tous les chiffres ou ["0-9"]
+end
+
+Begin
+Adresse de compte = Compte de balance
+end
+
+#Montant et monnaie
+begin
+def Tableau pour Montant=["0-9"+"x1"]
+print(entrer le montant et la monnaie:)
+end 
+
+#Mobile money
+begin
+def tableau pour numéro mobile money =["+78"+"143-77-88-1"]
+print(entrer le numéro mobile money:);
+
+#IBAN carte
+def tableau IBAN= ["AZ"+"1334-7888-9099-9899"]
+print(entrer l'IBAN:)
+
+#cryto monnaie
+def tableau pour cryto monnaie =["98dee5d99434491b89e0d7c58079e353"];
+print(entrer crypto monnaie:)
+
+#carte prépayée 
+def tableau de carte prépayée =["0888-6878-999-78"]
+print(entrer la carte prepayée:)
+
+#perfect money
+def tableau de perfect money = ["974479700874"]
+print(entrer le tableau de perfect money:)
+
+#message
+def boite de message=Montant envoyé"tableau pour montant"
+print(le montant envoyé est "")
+
+#Button transfert
+begin 
+def button Envoie = Transfert
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
